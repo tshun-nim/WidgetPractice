2c3b53ae56d76d5d074e7efcd6f6d1a880b9630c
tshun <tajimas0811@gmail.com>
2024-10-31 23:26:37 +0900

    Initial Commit

---
+++ WidgetPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetPractice
+//
+//  Created by 田島隼也 on 2024/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetPractice/WidgetPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetPracticeApp.swift
+//  WidgetPractice
+//
+//  Created by 田島隼也 on 2024/10/31.
+//
+
+import SwiftUI
+
+@main
+struct WidgetPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
